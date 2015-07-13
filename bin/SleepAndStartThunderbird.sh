d0d72a8d525fe2976ce15d592edda18d53af5d32
Julian Moosmann <moosmann@kth.se>
2015-07-13 18:19:26 +0200

    Add subfolder ~/bin.

---
+++ bin/SleepAndStartThunderbird.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Sleep 20 s to ensure internet connection is established before thunderbird starts.
+
+/bin/sleep 20 && /usr/bin/thunderbird
---
+++ bin/runMatlabConsole.sh
@@ -0,0 +1,3 @@
+LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 LD_LIBRARY_PATH=:/usr/local/lib:/usr/local/cuda/lib64:/usr/local/astra/lib:$LD_LIBRARY_PATH /usr/local/bin/matlab -nosplash -nodesktop -nodisplay
+#LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 LD_LIBRARY_PATH=/usr/local/astra/lib:/usr/local/cuda/lib64:$LD_LIBRARY_PATH /usr/local/bin/matlab -nosplash
+
